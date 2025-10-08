cb4b87fc5a6432ec6003f7b26185dace35f06b88
Ruben <r.bautista.2021@alumnos.urjc.es>
2025-10-08 12:58:19 +0200

    Servidor bloqueante mejorado

--- practicas/p1/RubenBautista/ServidorSimpleNoBloqueante/client.c
+++ practicas/p1/RubenBautista/ServidorSimpleNoBloqueante/client.c
@@ -1,8 +1,3 @@
-/*
- * Non-blocking TCP Client - Systems Distributed and Concurrent
- * Client with non-blocking communication and 0.5s timeout
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,22 +7,13 @@
 #include <sys/select.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <errno.h>
 
 #define SERVER_IP "127.0.0.1"
 #define PORT 8080
 #define BUFFER_SIZE 1024
-#define CLIENT_TIMEOUT_US 500000  /* 0.5 seconds in microseconds */
 
-/* Global variable for signal handler */
 int client_socket = -1;
 
-/* Function prototypes */
-void handle_signal(int sig);
-int setup_client_socket(void);
-void handle_server_communication(int sock_fd);
-
-/* Handle CTRL+C signal for graceful shutdown */
 void handle_signal(int sig) {
     (void)sig;
     printf("\nDisconnecting from server...\n");
@@ -37,45 +23,34 @@ void handle_signal(int sig) {
     exit(0);
 }
 
-/* Set up and connect client socket */
-int setup_client_socket(void) {
+int main() {
     struct sockaddr_in server_addr;
-    int sock_fd;
-    
-    /* Create TCP socket */
-    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_fd == -1) {
-        perror("socket");
-        return -1;
-    }
-    
-    /* Configure server address structure */
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    
-    /* Convert IP address from text to binary form */
-    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
-        perror("inet_pton");
-        close(sock_fd);
-        return -1;
-    }
-    
-    /* Connect to server */
-    if (connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
-        perror("connect");
-        close(sock_fd);
-        return -1;
-    }
-    
-    return sock_fd;
-}
-
-/* Handle communication with server using non-blocking recv with timeout */
-void handle_server_communication(int sock_fd) {
     char buffer[BUFFER_SIZE];
     fd_set read_fds;
     int max_fd, activity;
     struct timeval timeout;
+    
+    setbuf(stdout, NULL);
+    
+    signal(SIGINT, handle_signal);
+    
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    printf("Socket successfully created...\n");
+    
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    
+    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
+        perror("connect");
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
+    printf("Connected to the server...\n");
     
     printf("> ");
     fflush(stdout);
@@ -83,15 +58,17 @@ void handle_server_communication(int sock_fd) {
     while (1) {
         FD_ZERO(&read_fds);
         FD_SET(STDIN_FILENO, &read_fds);
-        FD_SET(sock_fd, &read_fds);
+        FD_SET(client_socket, &read_fds);
         
-        max_fd = (STDIN_FILENO > sock_fd) ? STDIN_FILENO : sock_fd;
+        if (STDIN_FILENO > client_socket) {
+            max_fd = STDIN_FILENO;
+        } else {
+            max_fd = client_socket;
+        }
         
-        /* Set timeout for select - 0.5 seconds as required */
         timeout.tv_sec = 0;
-        timeout.tv_usec = CLIENT_TIMEOUT_US;
+        timeout.tv_usec = 500000;
         
-        /* Wait for activity on input descriptors with timeout */
         activity = select(max_fd + 1, &read_fds, NULL, NULL, &timeout);
         
         if (activity < 0) {
@@ -99,9 +76,8 @@ void handle_server_communication(int sock_fd) {
             break;
         }
         
-        /* Handle incoming message from server with NON-BLOCKING recv */
-        if (FD_ISSET(sock_fd, &read_fds)) {
-            int bytes_received = recv(sock_fd, buffer, BUFFER_SIZE - 1, MSG_DONTWAIT);
+        if (FD_ISSET(client_socket, &read_fds)) {
+            int bytes_received = recv(client_socket, buffer, BUFFER_SIZE - 1, MSG_DONTWAIT);
             
             if (bytes_received > 0) {
                 buffer[bytes_received] = '\0';
@@ -110,51 +86,29 @@ void handle_server_communication(int sock_fd) {
                 printf("Server disconnected\n");
                 break;
             } else {
-                /* No data available or error */
-                if (errno != EAGAIN && errno != EWOULDBLOCK) {
-                    perror("recv");
-                    break;
-                }
+                printf("> ");
+                fflush(stdout);
             }
         }
         
-        /* Handle user input */
         if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+            printf("> ");
+            fflush(stdout);
+            
             if (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
                 buffer[strcspn(buffer, "\n")] = 0;
                 
-                if (send(sock_fd, buffer, strlen(buffer), 0) == -1) {
-                    perror("send");
+                if (send(client_socket, buffer, strlen(buffer), 0) == -1) {
+                    printf("Send failed - server disconnected\n");
                     break;
                 }
             }
         }
         
-        /* Always show prompt after each iteration (non-blocking behavior) */
         printf("> ");
         fflush(stdout);
     }
-}
-
-int main() {
-    /* Set output buffering for immediate display */
-    setbuf(stdout, NULL);
     
-    /* Register signal handler for CTRL+C */
-    signal(SIGINT, handle_signal);
-    
-    /* Set up client socket and connect to server */
-    client_socket = setup_client_socket();
-    if (client_socket == -1) {
-        exit(EXIT_FAILURE);
-    }
-    printf("Socket successfully created...\n");
-    printf("Connected to the server...\n");
-    
-    /* Handle communication with server */
-    handle_server_communication(client_socket);
-    
-    /* Cleanup */
     close(client_socket);
     return 0;
 }
--- practicas/p1/RubenBautista/ServidorSimpleNoBloqueante/server.c
+++ practicas/p1/RubenBautista/ServidorSimpleNoBloqueante/server.c
@@ -1,8 +1,3 @@
-/*
- * Non-blocking TCP Server - Systems Distributed and Concurrent
- * Monoclient server with non-blocking communication
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,68 +13,42 @@
 #define BUFFER_SIZE 1024
 #define MAX_CLIENTS 1
 
-/* Global variables for signal handler */
 int server_socket = -1;
-int client_socket = -1;
+int connection_socket = -1;
+int should_exit = 0;
 
-/* Function prototypes */
-void handle_signal(int sig);
-int setup_server_socket(void);
-int accept_client_connection(int server_fd);
-void handle_client_communication(int client_fd);
-void cleanup_resources(void);
-
-/* Handle CTRL+C signal for graceful shutdown */
 void handle_signal(int sig) {
     (void)sig;
-    printf("\nShutting down server...\n");
-    cleanup_resources();
-    exit(0);
+    should_exit = 1;
 }
 
-/* Clean up all resources */
-void cleanup_resources(void) {
-    if (client_socket != -1) {
-        close(client_socket);
-    }
-    if (server_socket != -1) {
-        close(server_socket);
-    }
-}
-
-/* Set up and configure server socket */
 int setup_server_socket(void) {
     struct sockaddr_in server_addr;
     int sock_fd;
     const int enable = 1;
     
-    /* Create TCP socket */
     sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_fd == -1) {
         perror("socket");
         return -1;
     }
     
-    /* Set socket option to reuse address */
     if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0) {
         perror("setsockopt");
         close(sock_fd);
         return -1;
     }
     
-    /* Configure server address structure */
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
     
-    /* Bind socket to specific port */
     if (bind(sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
         perror("bind");
         close(sock_fd);
         return -1;
     }
     
-    /* Listen for incoming connections */
     if (listen(sock_fd, MAX_CLIENTS) != 0) {
         perror("listen");
         close(sock_fd);
@@ -89,7 +58,6 @@ int setup_server_socket(void) {
     return sock_fd;
 }
 
-/* Accept a client connection */
 int accept_client_connection(int server_fd) {
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
@@ -104,58 +72,57 @@ int accept_client_connection(int server_fd) {
     return client_fd;
 }
 
-/* Handle communication with a connected client using non-blocking recv */
 void handle_client_communication(int client_fd) {
     char buffer[BUFFER_SIZE];
     fd_set read_fds;
     int max_fd, activity;
     int client_connected = 1;
+    struct timeval timeout;
     
     printf("> ");
     fflush(stdout);
     
-    while (client_connected) {
+    while (client_connected && should_exit == 0) {
         FD_ZERO(&read_fds);
         FD_SET(STDIN_FILENO, &read_fds);
         FD_SET(client_fd, &read_fds);
         
-        max_fd = (STDIN_FILENO > client_fd) ? STDIN_FILENO : client_fd;
+        if (STDIN_FILENO > client_fd) {
+            max_fd = STDIN_FILENO;
+        } else {
+            max_fd = client_fd;
+        }
         
-        /* Wait for activity on input descriptors - no timeout for server */
-        activity = select(max_fd + 1, &read_fds, NULL, NULL, NULL);
+        timeout.tv_sec = 1;  /* 1 segundo de timeout para servidor */
+        timeout.tv_usec = 0;
+        
+        activity = select(max_fd + 1, &read_fds, NULL, NULL, &timeout);
         
         if (activity < 0) {
             perror("select");
             break;
         }
         
-        /* Handle incoming message from client with NON-BLOCKING recv */
+        if (activity == 0) {
+            /* Timeout - no tiene nada que leer del socket */
+            printf("> ");
+            fflush(stdout);
+            continue;
+        }
+        
         if (FD_ISSET(client_fd, &read_fds)) {
             int bytes_received = recv(client_fd, buffer, BUFFER_SIZE - 1, MSG_DONTWAIT);
             
             if (bytes_received > 0) {
                 buffer[bytes_received] = '\0';
                 printf("\n+++ %s\n", buffer);
-                printf("> ");
-                fflush(stdout);
             } else if (bytes_received == 0) {
                 printf("Client disconnected\n");
                 client_connected = 0;
                 break;
-            } else {
-                /* No data available - show prompt again */
-                if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                    printf("> ");
-                    fflush(stdout);
-                } else {
-                    perror("recv");
-                    client_connected = 0;
-                    break;
-                }
             }
         }
         
-        /* Handle user input */
         if (FD_ISSET(STDIN_FILENO, &read_fds)) {
             printf("> ");
             fflush(stdout);
@@ -172,17 +139,12 @@ void handle_client_communication(int client_fd) {
         }
     }
 }
-
 int main() {
-    /* Set output buffering for immediate display */
     setbuf(stdout, NULL);
     
-    /* Register signal handler for CTRL+C */
     signal(SIGINT, handle_signal);
     
-    /* Main server loop - accept new clients after disconnections */
-    while (1) {
-        /* Set up server socket for each new client */
+    while (should_exit == 0) {
         server_socket = setup_server_socket();
         if (server_socket == -1) {
             exit(EXIT_FAILURE);
@@ -190,28 +152,30 @@ int main() {
         printf("Socket successfully created...\n");
         printf("Socket successfully binded...\n");
         printf("Server listening...\n");
-        
-        /* Accept client connection */
-        client_socket = accept_client_connection(server_socket);
-        if (client_socket == -1) {
+
+        connection_socket = accept_client_connection(server_socket);
+        if (connection_socket == -1) {
             close(server_socket);
             continue;
         }
         
-        /* Close listening socket to reject new connections while serving current client */
         close(server_socket);
         server_socket = -1;
         
-        /* Handle communication with client */
-        handle_client_communication(client_socket);
+        handle_client_communication(connection_socket);
         
-        /* Cleanup client connection and wait for new one */
-        close(client_socket);
-        client_socket = -1;
+        close(connection_socket);
+        connection_socket = -1;
+        
         printf("Waiting for new client connection...\n");
     }
     
-    /* Cleanup (should not be reached due to signal handler) */
-    cleanup_resources();
+    if (connection_socket != -1) {
+        close(connection_socket);
+    }
+    if (server_socket != -1) {
+        close(server_socket);
+    }
+    printf("\nShutting down server...\n");
     return 0;
 }
